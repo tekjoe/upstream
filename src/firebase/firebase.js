6539d874debbf024e7cf7ec44d207847ec00bb88
tekjoe <ramirez.josephh@gmail.com>
2019-04-10 12:08:22 -0500

    removed firebase config settings

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBk3G5LAkGlopFjOttIaMr9t40FogBGT04",
-  authDomain: "stream-connector.firebaseapp.com",
-  databaseURL: "https://stream-connector.firebaseio.com",
-  projectId: "stream-connector",
-  storageBucket: "stream-connector.appspot.com",
-  messagingSenderId: "652293291841"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
